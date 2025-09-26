f0ff40825efbef87016de374c3b31aaec4dfe9da
Abel Okagbare <okagbareabel@gmail.com>
2025-09-26 21:04:07 +0100

    Cloudinary for media storage

--- multimedia_site/settings.py
+++ multimedia_site/settings.py
@@ -138,6 +138,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
